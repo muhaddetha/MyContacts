448221b187340daeda3482e935fd4db9e82c4ce7
muhaddetha <muhaddetha2@gmail.com>
2025-03-31 03:32:05 +0330

    Add files via upload

---
+++ SQL Server Scripts1/SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿Select * from MyContacts
+--select Name, Family From MyContacts
+--select * from MyContacts where Name =N'ایمان'
+--select * from MyContacts where Age>25 or Age<=30
+--insert into MyContacts (Name, Family, Mobile, Email, Age) values (N'علی',N'اکبری','0935******', 'ALI@yahoo.com', 12)
+--update MyContacts set Age=11 where ContactID=3
+--delete from MyContacts where ContactID=3
+--select * from MyContacts
